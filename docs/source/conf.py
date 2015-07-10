ec435fd3867e1e81bacf80c0b37b1c30dccd6657
jlaura <jlaura@asu.edu>
2015-07-10 11:36:45 -0700

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -280,7 +280,7 @@ texinfo_documents = [
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {'http://docs.python.org/': None}
 
-from unittest.mock import MagicMock
+from mock import Mock as MagicMock
 
 class Mock(MagicMock):
     @classmethod
